0a0e15cdeb744cb655e04fd01045f523dcacdebd
priya0302 <priyamanoharan0302@gmail.com>
2023-05-08 23:16:00 +0530

    carous

--- src/components/carous.js
+++ src/components/carous.js
@@ -1,20 +1,23 @@
 import { useEffect, useState } from "react";
 import img1 from "./carousimg1.jpg";
+import img2 from "./carousimg2.jpg";
 import img3 from "./carousimg3.jpg";
+
+
 function Carous() {
 
   const [selectedImage, setSelectedImage] = useState(0)
-  const [allImages] = useState([img1, img3])
+  const [allImages] = useState([img1, img2,img3])
   
   useEffect(() => {
     setInterval(() => {
-      setSelectedImage(selectedImage => selectedImage < 3 ? selectedImage + 1 : 0)
+      setSelectedImage(selectedImage => selectedImage < 2 ? selectedImage + 1 : 0)
     }, 5500)
   }, [])
 
   return (
     <div>
-      <img width={1513} height={450} src={allImages[selectedImage]}  alt="image1,image3" /> <br />
+      <img  class="center" src={allImages[selectedImage]}  alt="image1,image3" /> <br />
      
     </div>
   );
