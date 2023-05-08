46fcea643c781e8aa247df8c2607a8455a90f0f6
Naveenkumar S E <naveen42moon@gmail.com>
2023-05-08 08:13:08 +0530

    firebase-auth-added

--- src/components/sign.js
+++ src/components/sign.js
@@ -1,60 +1,85 @@
- import React from "react";
- import * as Components from './Components';
+import React, { useState } from "react";
+import {signInWithEmailAndPassword,createUserWithEmailAndPassword} from 'firebase/auth'
+import * as Components from './Components';
+import { auth } from '../firebase'
+function Sign() {
+    const [signIn, toggle] = React.useState(true);
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const onSignIn = (e) => {
+        e.preventDefault();
+        signInWithEmailAndPassword(auth,email,password)
+        .then((userCredential)=>{
+            console.log(userCredential)
+            window.location = '/'
+        }).catch((error)=>{
+            console.log(error);
+            alert("User Not Found");
+        })
+    }
 
- function Sign() {
-     const [signIn, toggle] = React.useState(true);
-      return(
+    const onSignUp = (e) =>{
+            e.preventDefault();
+            createUserWithEmailAndPassword(auth,email,password)
+            .then((userCredential)=>{
+                console.log(userCredential)
+            }).catch((error)=>{
+                console.log(error);
+            })
+    }
+    return (
         <div className="sign">
-          <Components.Container>
-              <Components.SignUpContainer signinIn={signIn}>
-                  <Components.Form>
-                      <Components.Title>Create Account</Components.Title>
-                      <Components.Input type='text' placeholder='Name' />
-                      <Components.Input type='email' placeholder='Email' />
-                      <Components.Input type='password' placeholder='Password' />
-                      <Components.Button>Sign Up</Components.Button>
-                  </Components.Form>
-              </Components.SignUpContainer>
+            <Components.Container>
+                <Components.SignUpContainer signinIn={signIn}>
+                    <Components.Form>
+                        <Components.Title>Create Account</Components.Title>
+                        <Components.Input type='text' placeholder='Name' value={name} onChange={(e) => setName(e.target.value)} />
+                        <Components.Input type='email' placeholder='Email' value={email} onChange={(e) => setEmail(e.target.value)} />
+                        <Components.Input type='password' placeholder='Password' value={password} onChange={(e) => setPassword(e.target.value)} />
+                        <Components.Button onClick={onSignUp}>Sign Up</Components.Button>
+                    </Components.Form>
+                </Components.SignUpContainer>
 
-              <Components.SignInContainer signinIn={signIn}>
-                   <Components.Form>
-                       <Components.Title>Sign in</Components.Title>
-                       <Components.Input type='email' placeholder='Email' />
-                       <Components.Input type='password' placeholder='Password' />
-                       <Components.Anchor href='#'>Forgot your password?</Components.Anchor>
-                       <Components.Button>Sigin In</Components.Button>
-                   </Components.Form>
-              </Components.SignInContainer>
+                <Components.SignInContainer signinIn={signIn}>
+                    <Components.Form >
+                        <Components.Title>Sign in</Components.Title>
+                        <Components.Input type='email' placeholder='Email' value={email} onChange={(e) => setEmail(e.target.value)} />
+                        <Components.Input type='password' placeholder='Password' value={password} onChange={(e) => setPassword(e.target.value)} />
+                        <Components.Anchor href='#'>Forgot your password?</Components.Anchor>
+                        <Components.Button onClick={onSignIn}>Sigin In</Components.Button>
+                    </Components.Form>
+                </Components.SignInContainer>
 
-              <Components.OverlayContainer signinIn={signIn}>
-                  <Components.Overlay signinIn={signIn}>
+                <Components.OverlayContainer signinIn={signIn}>
+                    <Components.Overlay signinIn={signIn}>
 
-                  <Components.LeftOverlayPanel signinIn={signIn}>
-                      <Components.Title>Welcome Back!</Components.Title>
-                      <Components.Paragraph>
-                          To keep connected with us please login with your personal info
-                      </Components.Paragraph>
-                      <Components.GhostButton onClick={() => toggle(true)}>
-                          Sign In
-                      </Components.GhostButton>
-                      </Components.LeftOverlayPanel>
+                        <Components.LeftOverlayPanel signinIn={signIn}>
+                            <Components.Title>Welcome Back!</Components.Title>
+                            <Components.Paragraph>
+                                To keep connected with us please login with your personal info
+                            </Components.Paragraph>
+                            <Components.GhostButton onClick={() => toggle(true)}>
+                                Sign In
+                            </Components.GhostButton>
+                        </Components.LeftOverlayPanel>
 
-                      <Components.RightOverlayPanel signinIn={signIn}>
-                        <Components.Title>Hello, Friend!</Components.Title>
-                        <Components.Paragraph>
-                            Enter Your personal details and start journey with us
-                        </Components.Paragraph>
+                        <Components.RightOverlayPanel signinIn={signIn}>
+                            <Components.Title>Hello, Friend!</Components.Title>
+                            <Components.Paragraph>
+                                Enter Your personal details and start journey with us
+                            </Components.Paragraph>
                             <Components.GhostButton onClick={() => toggle(false)}>
                                 Sigin Up
-                            </Components.GhostButton> 
-                      </Components.RightOverlayPanel>
-  
-                  </Components.Overlay>
-              </Components.OverlayContainer>
+                            </Components.GhostButton>
+                        </Components.RightOverlayPanel>
 
-          </Components.Container>
-          </div>
-      )
- }
+                    </Components.Overlay>
+                </Components.OverlayContainer>
 
- export default Sign;
+            </Components.Container>
+        </div>
+    )
+}
+
+export default Sign;
--- src/firebase.js
+++ src/firebase.js
@@ -1,7 +1,8 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import {getFirestore} from "firebase/firestore";
-import {getStorage} from 'firebase/storage'
+import {getStorage} from 'firebase/storage';
+import { getAuth } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -17,6 +18,7 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
 
 export const storage = getStorage();
 export const db = getFirestore(app);
