ab64c2b3dbd04e53b5eb8c1e2cf4808a462485c0
priya0302 <priyamanoharan0302@gmail.com>
2023-05-13 23:04:19 +0530

    updated

--- src/pages/admin_page/view_products/view_products.js
+++ src/pages/admin_page/view_products/view_products.js
@@ -8,9 +8,13 @@ import { useState } from "react";
 import ViewList from "./view_list";
 
 const ViewProducts = () => {
-    const category = ["cards", "paper"]
+    const category = ["cards", "paper","stickers","covers","bondsheet","flex"]
     const [cards, setCards] = useState([]);
-    const [papers, setPaper] = useState([]);
+    const [paper, setPaper] = useState([]);
+    const [stickers, setSticker] = useState([]);
+    const [covers, setCover] = useState([]);
+    const [bondsheet, setBondsheet] = useState([]);
+    const [flex, setFlex] = useState([]);
     useEffect(() => {
         const q = query(collection(db, 'cards'))
         const unSubscribe = onSnapshot(q, (querySnapshot) => {
@@ -30,6 +34,50 @@ const ViewProducts = () => {
                 todoArr.push({ ...doc.data(), id: doc.id })
             });
             setPaper(todoArr);
+        })
+        return () => unSubscribe();
+    }, [])
+    useEffect(() => {
+        const q = query(collection(db, 'stickers'))
+        const unSubscribe = onSnapshot(q, (querySnapshot) => {
+            let todoArr = []
+            querySnapshot.forEach((doc) => {
+                todoArr.push({ ...doc.data(), id: doc.id })
+            });
+            setSticker(todoArr);
+        })
+        return () => unSubscribe();
+    }, [])
+    useEffect(() => {
+        const q = query(collection(db, 'covers'))
+        const unSubscribe = onSnapshot(q, (querySnapshot) => {
+            let todoArr = []
+            querySnapshot.forEach((doc) => {
+                todoArr.push({ ...doc.data(), id: doc.id })
+            });
+            setCover(todoArr);
+        })
+        return () => unSubscribe();
+    }, [])
+    useEffect(() => {
+        const q = query(collection(db, 'bond sheet'))
+        const unSubscribe = onSnapshot(q, (querySnapshot) => {
+            let todoArr = []
+            querySnapshot.forEach((doc) => {
+                todoArr.push({ ...doc.data(), id: doc.id })
+            });
+            setBondsheet(todoArr);
+        })
+        return () => unSubscribe();
+    }, [])
+    useEffect(() => {
+        const q = query(collection(db, 'flex'))
+        const unSubscribe = onSnapshot(q, (querySnapshot) => {
+            let todoArr = []
+            querySnapshot.forEach((doc) => {
+                todoArr.push({ ...doc.data(), id: doc.id })
+            });
+            setFlex(todoArr);
         })
         return () => unSubscribe();
     }, [])
@@ -70,8 +118,88 @@ const ViewProducts = () => {
                         </tr>
                     </thead>
                     <tbody class="bg-grey-light flex flex-col items-center justify-between overflow-y-scroll w-full" style={{ height: "50vh" }}>
-                        {papers.map((card, i) => {
-                            return <ViewList key={i} index={i} product={card} pcategory={category[1]} />
+                        {paper.map((paper, i) => {
+                            return <ViewList key={i} index={i} product={paper} pcategory={category[1]} />
+                        })}
+                    </tbody>
+                </table>
+            </div>
+            <h1>stickers</h1>
+            <div class="container ml-24 w-full mt-20">
+                <table class="text-left w-full">
+                    <thead class="bg-black flex text-white w-full">
+                        <tr class="flex w-full mb-4">
+                            <th class="p-4 w-1/8">S.No</th>
+                            <th class="p-4 w-1/6">Name</th>
+                            <th class="p-4 w-1/5">Category</th>
+                            <th class="p-4 w-1/6">Desc</th>
+                            <th class="p-4 w-1/5">Prize</th>
+                            <th class="p-4 w-1/5">Size</th>
+                        </tr>
+                    </thead>
+                    <tbody class="bg-grey-light flex flex-col items-center justify-between overflow-y-scroll w-full" style={{ height: "50vh" }}>
+                        {stickers.map((sticker, i) => {
+                            return <ViewList key={i} index={i} product={sticker} pcategory={category[2]} />
+                        })}
+                    </tbody>
+                </table>
+            </div>
+            <h1>covers</h1>
+            <div class="container ml-24 w-full mt-20">
+                <table class="text-left w-full">
+                    <thead class="bg-black flex text-white w-full">
+                        <tr class="flex w-full mb-4">
+                            <th class="p-4 w-1/8">S.No</th>
+                            <th class="p-4 w-1/6">Name</th>
+                            <th class="p-4 w-1/5">Category</th>
+                            <th class="p-4 w-1/6">Desc</th>
+                            <th class="p-4 w-1/5">Prize</th>
+                            <th class="p-4 w-1/5">Size</th>
+                        </tr>
+                    </thead>
+                    <tbody class="bg-grey-light flex flex-col items-center justify-between overflow-y-scroll w-full" style={{ height: "50vh" }}>
+                        {covers.map((cover, i) => {
+                            return <ViewList key={i} index={i} product={cover} pcategory={category[3]} />
+                        })}
+                    </tbody>
+                </table>
+            </div>
+            <h1>Bondsheet</h1>
+            <div class="container ml-24 w-full mt-20">
+                <table class="text-left w-full">
+                    <thead class="bg-black flex text-white w-full">
+                        <tr class="flex w-full mb-4">
+                            <th class="p-4 w-1/8">S.No</th>
+                            <th class="p-4 w-1/6">Name</th>
+                            <th class="p-4 w-1/5">Category</th>
+                            <th class="p-4 w-1/6">Desc</th>
+                            <th class="p-4 w-1/5">Prize</th>
+                            <th class="p-4 w-1/5">Size</th>
+                        </tr>
+                    </thead>
+                    <tbody class="bg-grey-light flex flex-col items-center justify-between overflow-y-scroll w-full" style={{ height: "50vh" }}>
+                        {bondsheet.map((bondsheet, i) => {
+                            return <ViewList key={i} index={i} product={bondsheet} pcategory={category[4]} />
+                        })}
+                    </tbody>
+                </table>
+            </div>
+            <h1>Flex</h1>
+            <div class="container ml-24 w-full mt-20">
+                <table class="text-left w-full">
+                    <thead class="bg-black flex text-white w-full">
+                        <tr class="flex w-full mb-4">
+                            <th class="p-4 w-1/8">S.No</th>
+                            <th class="p-4 w-1/6">Name</th>
+                            <th class="p-4 w-1/5">Category</th>
+                            <th class="p-4 w-1/6">Desc</th>
+                            <th class="p-4 w-1/5">Prize</th>
+                            <th class="p-4 w-1/5">Size</th>
+                        </tr>
+                    </thead>
+                    <tbody class="bg-grey-light flex flex-col items-center justify-between overflow-y-scroll w-full" style={{ height: "50vh" }}>
+                        {flex.map((flex, i) => {
+                            return <ViewList key={i} index={i} product={flex} pcategory={category[5]} />
                         })}
                     </tbody>
                 </table>
